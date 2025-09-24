3afeb4d1b791108943c56ccf7a8c86c8d2b3a5bb
paulina-pacheco-o <paulimichi05@gmail.com>
2025-09-25 00:04:46 +0200

    detail page movie

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie }) => {
-  const { title, image, genre } = movie;
+  const { id, title, image, genre } = movie;
   return (
     <div className="col-12 col-md-6 col-lg-4">
-      <div className="card-movie">
-        <img src={image} className='cover-movie-card' alt={title} />
-      </div>
+      <Link to={`/movies/${id}`}>
+        <div className="card-movie">
+          <img src={image} className='cover-movie-card' alt={title} />
+        </div>
+      </Link>
     </div>
   )
 }
--- src/pages/DetailsMovie.jsx
+++ src/pages/DetailsMovie.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const DetailsMovie = () => {
+  const { id } = useParams();
+
+  const [movie, setMovie] = useState([]);
+
+  const fetchMovies = () => {
+    axios.get(`http://localhost:3000/api/movies/${id}`).then((resp) => {
+      setMovie(resp.data);
+    })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(fetchMovies, [])
   return (
-    <div>
-      <h2>Dettaglio film</h2>
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <div className="detail-card">
+            <div className="d-flex">
+              <div className="cover-image">
+                <img src={movie.image} alt={movie.title} />
+              </div>
+              <div className="text-details text-center mt-3">
+                <h1>{movie.title}</h1>
+                <p>{movie.director}</p>
+                <p>{movie.genre}</p>
+                <p>{movie.release_year}</p>
+                <p>{movie.abstract}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
