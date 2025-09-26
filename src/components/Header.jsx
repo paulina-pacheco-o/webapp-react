34f3c150b598252d837872b083567c91093605d4
paulina-pacheco-o <paulimichi05@gmail.com>
2025-09-26 17:28:29 +0200

    upd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <div className="col-12 d-flex justify-content-between">
           <h1>Boolmovies</h1>
           <Link className="btn btn-add text-center" to="/movies/create">
-            Aggiungi un nuovo libro
+            Aggiungi un nuovo film
           </Link>
         </div>
       </div>
