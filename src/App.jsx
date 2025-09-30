8e98df5e68cfa1dab712ad6e04e68d9b169e0fe6
paulina-pacheco-o <paulimichi05@gmail.com>
2025-09-30 23:17:35 +0200

    context provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,23 @@ import Homepage from "./pages/Homepage";
 import DetailsMovie from "./pages/DetailsMovie";
 import CreateMovie from "./pages/CreateMovie";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalContext from "./contexts/globalContext";
+import { useState } from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/movies/:id" element={<DetailsMovie />} />
-          <Route path="/movies/create" element={<CreateMovie />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <GlobalContext.Provider value={{ isLoading, setIsLoading }}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/movies/:id" element={<DetailsMovie />} />
+            <Route path="/movies/create" element={<CreateMovie />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </GlobalContext.Provider>
   )
 }
 
